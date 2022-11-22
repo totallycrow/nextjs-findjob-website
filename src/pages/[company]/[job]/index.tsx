797b1cdc6cd506f0d4710af253e39ac543600017
Adrian Gajda <adrian.gajda@hotmail.com>
2022-11-22 14:52:24 +0000

    add data fetching for specific job pages

--- src/pages/[company]/[job]/index.tsx
+++ src/pages/[company]/[job]/index.tsx
@@ -1,55 +1,72 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import request, { gql } from "graphql-request";
 import React from "react";
+import {
+  ALL_JOBS,
+  CITIES,
+  CITIES_KEY,
+  JOBS_KEY,
+  endpoint,
+} from "../../../services/queries";
+import { IJobsData } from "../../../services/useGetJobs";
 
-const endpoint = "https://api.graphql.jobs/";
-const FILMS_QUERY = gql`
-  {
-    companies {
-      slug
-    }
-    jobs {
-      slug
-      company {
-        slug
-      }
-    }
-  }
-`;
-
-export const index = () => {
-  return <div>index</div>;
+export const index = (props: any) => {
+  console.log("************************************** component");
+  console.log(props.data);
+  const data = props.data;
+  return <div>{data[0].slug}</div>;
 };
 export default index;
 
-export async function getStaticProps() {
+export async function getStaticProps(context: any) {
   const queryClient = new QueryClient();
+  const params = context.params!;
+  const { company } = params;
+  const jobSlug = params.job;
 
-  await queryClient.prefetchQuery(["pages"], () => {
-    return request(endpoint, FILMS_QUERY);
+  await queryClient.prefetchQuery([JOBS_KEY], () => {
+    return request(endpoint, ALL_JOBS);
   });
+  console.log("///////////////////////////////////////////");
+  console.log(jobSlug);
+  console.log(params);
+  console.log(company);
 
   console.log(queryClient);
+  const jobsData = queryClient.getQueryData<IJobsData>([JOBS_KEY]);
+  console.log(jobsData);
+
+  if (!jobsData) return;
+  const filteredData = jobsData.jobs.filter((job) => {
+    if (job.company.slug === company && job.slug === jobSlug) {
+      return true;
+    } else {
+      return false;
+    }
+  });
 
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
+      data: filteredData,
     },
   };
 }
 
 export async function getStaticPaths() {
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(["pages"], () => {
-    return request(endpoint, FILMS_QUERY);
+
+  await queryClient.prefetchQuery([JOBS_KEY], () => {
+    return request(endpoint, ALL_JOBS);
   });
-  const data = queryClient.getQueryData(["pages"]);
-  console.log(data);
+  await queryClient.prefetchQuery([CITIES_KEY], () => {
+    return request(endpoint, CITIES);
+  });
 
-  console.log("static paths");
-  console.log(data.companies);
+  const jobsData = queryClient.getQueryData<IJobsData>([JOBS_KEY]);
+  if (!jobsData) return;
 
-  const paths = data.jobs.map((city: any) => {
+  const paths = jobsData.jobs.map((city) => {
     return {
       params: {
         company: city.company.slug,
